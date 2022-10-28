7b95a249a8817b498bb1833112949118371095ca
Hieu Dang <hieudang@Hieus-MacBook-Air.local>
2022-10-27 22:48:57 -0400

    fix mongoose

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () =>{
     try {
         await mongoose.connect(
             process.env.MONGO_URL,
-            
+            {useNewUrlParser: true}
         );
         console.log('MongoDB is Connected...')
     } catch (err) {
