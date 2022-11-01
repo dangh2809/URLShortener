d81a37655f46e1b2b4f4a9c8b553923fd1248106
Hieu Dang <hieudang@Hieus-MacBook-Air.local>
2022-10-31 22:53:19 -0400

    fixed url

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -4,7 +4,7 @@
 export const buildPath = () =>{
     
     if (process.env.NODE_ENV === "production"){
-        return `https://bittys.herokuapp.com/`
+        return `https://bittys.herokuapp.com`
     }
     else {
         return `http://localhost:8082`
